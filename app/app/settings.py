fd0d06d929c552a377323b13ffc3a996a7b6a573
Rajesh Sahani <Rajesh.Sahani+saks@saks.com>
2026-02-28 23:27:57 +0530

    updated githib actions

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-2fj2%m!azb$uho!9mg8pw!c09!v01hs70a@0zbkpawjakyv+2j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
 # Application definition
 
